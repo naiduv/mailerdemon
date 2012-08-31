fc3e0612145be6537d3fa6dba3b4a3b20a3d099a
naiduv <naiduv@bittorrent.com>
2012-08-31 07:08:33 +0000

    wip

--- fresume/upload_file.php
+++ fresume/upload_file.php
@@ -1,7 +1,7 @@
 <?php
 $allowedExts = array("pdf");
 $extension = end(explode(".", $_FILES["file"]["name"]));
-
+/*
 $ip=$_SERVER['REMOTE_ADDR'];
 echo "ip: " . $ip . "<br />" ;
 echo "Upload: " . $_FILES["file"]["name"] . "<br />";
@@ -9,6 +9,7 @@ echo "Type: " . $_FILES["file"]["type"] . "<br />";
 echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
 echo "Name: " . $_POST["id"] . "<br/>";
 echo "<br/>"."<br/>"."<br/>";
+*/
 
 if (($_FILES["file"]["type"] == "application/pdf")
 && ($_FILES["file"]["size"] < 100000)
@@ -16,26 +17,26 @@ if (($_FILES["file"]["type"] == "application/pdf")
   {
   if ($_FILES["file"]["error"] > 0)
     {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+      /*echo "Return Code: " . $_FILES["file"]["error"] . "<br />";*/
     }
   else
     {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+      /*    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
     echo "Type: " . $_FILES["file"]["type"] . "<br />";
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
     echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
+      */
     $filename = $_FILES["file"]["name"];
     $id = $_POST["id"];
     if (file_exists("upload/" . $id))
       {
-      echo $_FILES["file"]["name"] . " already exists. ";
+	echo $_FILES["file"]["name"] . " already exists. ";
       }
     else
       {
       move_uploaded_file($_FILES["file"]["tmp_name"],
       "upload/" . $id); 
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      echo "<a href='http://mailerdemon.com/fresume/" . $id . "'>Resume Link</a>";
       }
     }
   }
